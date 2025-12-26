4fb53669a3bd30c308633ebe3949349bc03deb70
mMashood19 <muhammadmashood.official@gmail.com>
2025-12-26 17:53:52 +0500

    Fix missing comma in middleware

--- form_management_system/settings.py
+++ form_management_system/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
